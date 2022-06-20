fa5c9e443e2d1706dc90473f9dde312d9c572716
Sujith M <sujithm@Sujiths-MBP.modem>
2022-06-21 00:14:46 +1000

    Initial commit

---
+++ customerapp/main/main.go
@@ -0,0 +1,21 @@
+package main
+
+type CustomerController struct {
+	// Explicit dependency that hides dependent logic
+	store domain.CustomerStore // CustomerStore value 
+}
+
+func (c CustomerController ) Add (c domain.Customer) { 
+	err:= c.store.Create(c)
+	if err!=nil { 
+		fmt.Println("Error:", err) 
+		return
+	}
+	fmt.Println("New Customer has been created")
+}
+
+func main() {
+	controller := CustomerController{
+	store : mapstore.NewMapStore(), // Inject the dependency
+	// store : mongodb.NewMongoStore(), // with another database
+}
